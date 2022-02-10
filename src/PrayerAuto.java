897803874b8a363661f1e84990a51d9e19fba8ee
AnthonyMastores <42610248+AnthonyMastores@users.noreply.github.com>
2022-02-09 21:09:58 -0600

    Add files via upload

---
+++ src/PrayerAuto.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.nio.charset.Charset;
+public class PrayerAuto {
+
+    public static void main(String[] args) {
+        printPrayerLines();
+    }
+    private static void printPrayerLines() {
+        String line;
+        try {
+            try (
+                    InputStream fis = new FileInputStream("C:\\Users\\Anthony\\eclipse-workspace\\PrayerHelper\\prayers.txt");
+                    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("Cp1252"));
+                    BufferedReader br = new BufferedReader(isr);
+            ) {
+                while ((line = br.readLine()) != null) {
+                    // Deal with the line
+                    System.out.println(line);
+                   
+
+                    try {
+                        System.out.print("\n\n");
+                       // Thread.sleep(10000);
+                        Thread.sleep(60000); // every 60 seconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+
